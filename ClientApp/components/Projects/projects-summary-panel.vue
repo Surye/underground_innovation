3c89fba62c4dba28e89cb058054ce4c49413820d
Vincent Castellano <surye80@gmail.com>
2018-02-13 23:23:48 -0800

    Fix link

--- ClientApp/components/Projects/projects-summary-panel.vue
+++ ClientApp/components/Projects/projects-summary-panel.vue
@@ -4,7 +4,7 @@
             <b-card>
                 <b-row>
                     <b-col cols=10>
-                        <h4><b-link :disabled="!project.member" :to="{ name: 'project_forums', params: { project_id: project.project_id }}">{{project.title}}</b-link></h4>
+                        <h4><b-link :disabled="!project.member" :to="{ name: 'project_details', params: { project_id: project.project_id }}">{{project.title}}</b-link></h4>
                     </b-col>
                     <b-col v-if="!project.member" cols=2>
                         <b-link>Join Project</b-link>
@@ -20,11 +20,11 @@
     </b-row>
 </template>
 
-<script> 
+<script>
 
 export default {
   props: {
-    project: Object    
+    project: Object
   },
   data() {
     return {
